934cac85d632bf3d857f9db04d3d6163fb0eb6f5
Yihong Pan <109496565+Evan02580@users.noreply.github.com>
2026-07-26 12:39:34 +0800

    Change data and output paths

--- model/config_liaoning.py
+++ model/config_liaoning.py
@@ -4,11 +4,11 @@ import os
 
 @dataclass
 class Config:
-    data_path: str = r"D:\Desktop\研究生\课题组\研究课题-电价预测\文献及数据集\辽宁数据集\GridPulse_Liaoning.csv"
+    data_path: str = r"./datasets/GridPulse_Liaoning.csv"
     sheet_name: str = "Sheet1"
     time_col: str = "Time"
     target_col: str = "Real-time price"
-    output_dir: str = r"D:\Desktop\山东电价预测\RHC_CDE_ARDiff\outputs_liaoning"
+    output_dir: str = r"./outputs_liaoning"
     market: str = "liaoning"
     train_ratio: float = 0.7
     val_ratio: float = 0.1
